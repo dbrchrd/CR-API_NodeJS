6a88e0e5b08c4aa730cc12c6e944989764656045
dbrchrd <83125728+dbrchrd@users.noreply.github.com>
2022-07-28 20:46:58 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,3 +6,12 @@ require("dotenv").config();
 const routes = require("./routes.js");
 let HOST = "127.0.0.1" || "0.0.0.0"; // Dev || Prod
 const PORT = 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use("/", routes);
+
+app.listen(PORT, HOST, () => {
+  console.log(`Listening in port ${PORT}`);
+});
